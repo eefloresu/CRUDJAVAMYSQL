5140b4a0a2db7d3104ec3c8ecf9d572d4859f2d8
eefloresu <ulloaelian215@gmail.com>
2024-10-06 13:30:03 -0600

    Primer commit del proyecto

---
+++ CRUDJAVAMYSQL/src/main/java/com/mycompany/crudjavamysql/CRUDJAVAMYSQL.java
@@ -0,0 +1,13 @@
+package com.mycompany.crudjavamysql;
+
+/**
+ * @author eefloresu@unah.hn
+ * @version 0.1.0
+ * @date 2024-11-06
+ */
+public class CRUDJAVAMYSQL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
